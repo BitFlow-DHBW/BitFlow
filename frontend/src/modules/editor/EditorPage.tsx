9720e924cc79ef87ab273b8cfef53fb0245f3b6f
Moritz Czekalski <moritz.czekalski@sew-eurodrive.de>
2026-05-12 16:41:33 +0200

    Pipeline-Test gefixt

--- frontend/src/modules/editor/EditorPage.tsx
+++ frontend/src/modules/editor/EditorPage.tsx
@@ -1,15 +1,1 @@
-import Canvas from "./components/Canvas";
-import SidePanel from "./components/SidePanel";
-
-function EditorPage() {
-  return (
-    <div className="flex h-screen">
-      <SidePanel />
-      <div className="flex-1 relative">
-        <Canvas />
-      </div>
-    </div>
-  );
-}
-
-export default EditorPage;
+export { EditorPage as default } from './pages/EditorPage';
